e0904fe3532581a14fe53d896c94953fb4a0b78f
Alf-Prado <alfpradoc@gmail.com>
2020-05-21 23:23:16 -0500

    Funcionalidad completa

---
+++ cypress/integration/my-tests/nafin-credito.spec.js
@@ -0,0 +1,37 @@
+describe('https://www.nafin.com/portalnf/content/herramientas-de-negocio/simulador-de-creditos/simulador-de-creditos.do', () => {
+    describe('Page loads', () => {
+        it('Should load the page', () => {
+            cy.visit('https://www.nafin.com/portalnf/content/herramientas-de-negocio/simulador-de-creditos/simulador-de-creditos.do')
+
+            cy.get('#dispDate')
+            .clear()
+            .type('05/05/2020')
+
+            cy.contains('Fecha de disposición').click()
+
+            cy.get('#creditAmount')
+            .clear()
+            .type(20000)
+
+            cy.get('#paymentMethod')
+            .select('1')
+
+            cy.get('#period')
+            .select('2')
+
+            cy.get('#rate')
+            .clear()
+            .type(15)
+
+            cy.contains('Calcular').click()
+
+            cy.contains('Ahora no').click()
+
+            cy.get('#resultadosSimulador')
+            .find('tbody')
+            .find('tr')
+            .its('length')
+            .should('eq', 24)
+        })
+    })
+})
